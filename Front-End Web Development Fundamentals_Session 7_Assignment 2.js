2cef9a7fda31dfc5152818f9b2447ac35df3639b
smanav <smanav1991@gmail.com>
2017-04-20 17:15:24 +0530

    Add files via upload

---
+++ Front-End Web Development Fundamentals_Session 7_Assignment 2.js
@@ -0,0 +1,15 @@
+                        // Addition method using passing function argument
+
+                
+                        function add ( a, b, c, d, e, f, g, h, i, j)
+
+                                               {
+
+                                               	 console.log( a + b + c);
+
+                                                 console.log( a + b + c + d + e + f + g + h + i + j);
+                                               
+                                               };
+
+                        add( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ); // calling a function add with arguments
+      
